d613912dcffb2f0011ac6c9ab33ddb2a02fb9e79
simmarum <mateusz0831@gmail.com>
2020-08-26 20:31:12 +0200

    Add timers and fix grid system

--- home/models.py
+++ home/models.py
@@ -9,6 +9,7 @@ from wagtail.admin.edit_handlers import FieldPanel, MultiFieldPanel, InlinePanel
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 
+import ast
 
 class HomePage(Page):
     def get_context(self, request):
@@ -21,6 +22,7 @@ class HomePage(Page):
 
 class Product(Page):
     sku = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
     short_description = models.TextField(blank=True, null=False)
     image = models.ForeignKey(
         'wagtailimages.Image',
@@ -35,6 +37,7 @@ class Product(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('sku'),
+        FieldPanel('category'),
         ImageChooserPanel('image'),
         FieldPanel('short_description'),
         FieldPanel('content_ingredients'),
@@ -44,6 +47,32 @@ class Product(Page):
 
     def get_content_ingredients_as_list(self):
         return self.content_ingredients.split('\n')
+
+    def get_content_additional_as_dict(self):
+        """
+        ..notes:
+            [
+                {
+                    'name': 'timer',
+                    'value': 10, # in seconds
+                },
+                {
+                    'name': 'opis',
+                    'value': 'Opis co się dzieje w danym kroku'
+                },
+            ]
+        """
+        available_things = [
+            'timer',
+            'opis'
+        ]
+        new_content_additional = []
+        if self.content_additional != '':
+            content_ingredients_py = ast.literal_eval(self.content_additional)
+            for one_piece in content_ingredients_py:
+                if one_piece['name'] in available_things:
+                    new_content_additional.append(one_piece)
+        return new_content_additional
 
     def get_context(self, request):
         context = super().get_context(request)
